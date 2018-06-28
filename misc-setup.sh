4f18a289c000540ef0031cd92269c06e9d245782
Michael Ruelas <michaelrue@zillowgroup.com>
2018-06-28 08:00:32 -0700

    Adding bower dependency, and composer packages for linting.

--- misc-setup.sh
+++ misc-setup.sh
@@ -11,6 +11,7 @@ npm install --g prettier
 npm install --g grunt
 npm install -g gulp
 npm install -g sails
+npm install -g bower
 
 #Dark Slack Theme
 cat slack-dark-theme.js >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
@@ -22,6 +23,10 @@ cd ~/Homestead
 git checkout v7.8.0
 bash init.sh
 vagrant plugin install vagrant-bindfs
+
+#Composer Packages
+composer global require "squizlabs/php_codesniffer=*"
+composer global require greynoise-design/laravel-coding-standard
 
 #ZSH install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
